021d5b459c0625ff9eb4190957610be57b446e7e
Nicolas <nico.hhara@gmail.com>
2024-08-16 11:13:45 -0300

    Aprendendo JavaScript

---
+++ sript.js
@@ -0,0 +1,14 @@
+//perguntando ao usuário
+let nome = prompt ("Digite seu nome")
+//alerta na tela 
+alert("Olá mundo");
+//console do navegado
+console.log("Teste de console")
+//inclui um html em um elemento identificado ID
+document.getElementById("demo").innerHTML="Nicolas Hara"
+
+
+
+
+
+
